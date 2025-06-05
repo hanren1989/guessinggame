7f56ac053bc8401fc95a2bed4b2a35cc81af4bea
hanren1989 <dingjianyi1989@sina.com>
2025-06-05 19:09:51 -0400

    add getguess function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-function getguess {
-	read -p "Enter a number:" guess;
-	regex='[^0-9]+$';
-	while [[ $guess =~ $regex ]] || [[ -z $guess ]]; do
-	 echo "ERROR: You need to enter a number."
-	 echo "(You have entered '$guess')";
-	 echo "Try again:";
-	 read -p "Enter a real number:" guess;
-	done
-}
+
 
 # count the files
 
@@ -16,17 +7,28 @@ numberofdir=$(ls | wc -w)
 
 echo "Guess how many files are there in the current directory?"
 
+
+function getguess {
+    local guess
+    while true; do
+        read -p "Enter a number: " guess
+        # test if the input is not a number
+        if [[ -z "$guess" ]] || [[ "$guess" =~ [^0-9] ]]; then
+            echo "ERROR: You need to enter a number."
+            echo "Try again."
+        else
+            if [[ $guess -eq $numberofdir ]]; then
+            	echo "Congratulations! You guessed it right!"
+            	exit 0
+    	    elif [[ $guess -lt $numberofdir ]]; then
+            	echo "Too low! Try again."
+            else
+            	echo "Too high! Try again."
+    	    fi
+  
+        fi
+    done
+}
+
 getguess
 
-while true; do
-    read -p "Your guess: " guess
-
-    if [[ $guess -eq $numberofdir ]]; then
-        echo "Congratulations! You guessed it right!"
-        exit 0
-    elif [[ $guess -lt $numberofdir ]]; then
-        echo "Too low! Try again."
-    else
-        echo "Too high! Try again."
-    fi
-done
