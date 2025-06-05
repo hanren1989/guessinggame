0f9fa3e8f8ab96fbdee92fe003bb64063c60a9b1
hanren1989 <dingjianyi1989@sina.com>
2025-06-04 20:23:34 -0400

    correct the code from guessing a random number to guessing the number of files

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# generate a random number (1-100)
-target=$((RANDOM % 100 + 1))
+# count the files
+numberofdir=$(ls | wc -w)
 
 echo "Guess a number between 1 and 100!"
 
 while true; do
     read -p "Your guess: " guess
 
-    if [[ $guess -eq $target ]]; then
+    if [[ $guess -eq $numberofdir ]]; then
         echo "Congratulations! You guessed it right!"
         exit 0
-    elif [[ $guess -lt $target ]]; then
+    elif [[ $guess -lt $numberofdir ]]; then
         echo "Too low! Try again."
     else
         echo "Too high! Try again."
